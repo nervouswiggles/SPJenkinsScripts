ce4e0db0ad56eff277b55d849375b6a54fd48826
nervouswiggles <danielredfern25@gmail.com>
2022-06-25 16:14:35 -0400

    Update setSystemConfiguration.sh

--- preRequisiteScripts/setSystemConfiguration.sh
+++ preRequisiteScripts/setSystemConfiguration.sh
@@ -1,9 +1,4 @@
-# This is thesshd server system-wide configuration file 
-## Should reside within /etc/ssh/sshd_config
-Include /etc/ssh/sshd_config.d/*.conf
-PasswordAuthentication yes
-PermitRootLogin no
-ChallengeResponseAuthentication no
-UsePAM yes
-AcceptEnv LANG LC_*
-Subsystem       sftp    /usr/lib/openssh/sftp-server
+# !/bin/sh
+
+wget https://github.com/nervouswiggles/SPJenkinsScripts/blob/main/preRequisiteScripts/sshd_config;
+sudo whoami;
